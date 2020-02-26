21f43fdf18fa2098b4bb1ad21f90d5ec977ef68f
Yukari-Matsuno <mareie.m.1005@gmail.com>
2020-02-26 21:14:07 +0900

    closetCycle design plan

---
+++ calender.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='UTF-8'>
+  <link href="style/stylesheet.css" rel="stylesheet">
+  <link href="style/normalize.css" rel="stylesheet">
+  <title>Closet Cycle</title>
+</head>
+
+<body>
+
+<?php
+
+// 現在の年月を取得
+$year = date('Y');
+$month = date('n');
+
+// 月末日を取得
+$last_day = date('j', mktime(0, 0, 0, $month + 1, 0, $year));
+
+$calendar = array();
+$j = 0;
+
+// 月末日までループ
+for ($i = 1; $i < $last_day + 1; $i++) {
+
+    // 曜日を取得
+    $week = date('w', mktime(0, 0, 0, $month, $i, $year));
+
+    // 1日の場合
+    if ($i == 1) {
+
+        // 1日目の曜日までをループ
+        for ($s = 1; $s <= $week; $s++) {
+
+            // 前半に空文字をセット
+            $calendar[$j]['day'] = '';
+            $j++;
+        }
+    }
+
+    // 配列に日付をセット
+    $calendar[$j]['day'] = $i;
+    $j++;
+
+    // 月末日の場合
+    if ($i == $last_day) {
+
+        // 月末日から残りをループ
+        for ($e = 1; $e <= 6 - $week; $e++) {
+
+            // 後半に空文字をセット
+            $calendar[$j]['day'] = '';
+            $j++;
+        }
+    }
+}
+
+
+?>
+
+  <h1><?php echo $year ?>年<?php echo $month ?>月のカレンダー</h1>
+  <br>
+  <br>
+  <table>
+    <tr>
+      <th>Sunday</th>
+      <th>Monday</th>
+      <th>Tuesday</th>
+      <th>Wednesday</th>
+      <th>Tursday</th>
+      <th>Friday</th>
+      <th>Saturday</th>
+    </tr>
+
+    <tr>
+      <?php $cnt = 0 ?>
+      <?php foreach ($calendar as $key => $value): ?>
+
+      <td>
+        <?php $cnt++ ?>
+        <?php echo $value['day'] ?>
+      </td>
+
+      <?php if ($cnt == 7): ?>
+    </tr>
+    <tr>
+      <?php $cnt = 0 ?>
+      <?php endif ?>
+
+      <?php endforeach ?>
+    </tr>
+  </table>
+</body>
+
+</html>
